42082f00ae1d526044b8366e87eca82a8efe6b74
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-06-27 16:30:43 -0400

    making progress with the code 4:30 6:27

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,15 +4,14 @@ $(document).ready(function () {
     //OBJECT
     //crate player object 
     var player = {
-        startingAttack: 0,
+        hit: 0,
         health: 0,
         attack: 0,
     }
 
     var enemy = {
-        startingAttack: 0,
+        hit: 0,
         health: 0,
-        attack: 0,
     }
 
     var game = {
@@ -22,14 +21,20 @@ $(document).ready(function () {
         userChar: [$("#gokuchar"), $("#vegetachar"), $("#friezachar"), $("#buuchar")],
         enCharts: [$("#gokuen"), $("#vegetaen"), $("#friezaen"), $("#buuen")],
         defCharts: [$("#gokudef"), $("#vegetadef"), $("#friezadef"), $("#buudef")],
-        attack : $("#attack"),
-        fightText : $("#fight"),
+        attack: $("#attack"),
+        fightText: $("#fight"),
+        reset: $("#reset"),
         //Functions
         //==============================================================================================================================================
-        startplayer: function () {
+        startGame: function () {
             this.enemies.hide();
             this.defenders.hide();
-
+            this.reset.hide();
+            enemy.hit = 0;
+            enemy.health = 0;
+            player.hit = 0;
+            player.health = 0;
+            player.attack = 0;
         },
         selectGoku: function () {
             this.userChar[1].hide();
@@ -93,10 +98,31 @@ $(document).ready(function () {
         },
 
         fight: function () {
-           
-            enemy.health -= player.attack;
+            enemy.health -= player.hit;
+            player.hit += player.attack
+            console.log(enemy.health);
+            console.log(player.hit);
+            player.health -= enemy.hit;
+            console.log(player.health)
             // game.fightText.text(enemy.health)
-           
+            if (enemy.health <= 0) {
+                console.log("pick a new character")
+                this.defenders.hide();
+                this.attack.attr("disabled", true);
+
+            } else if (player.health <= 0) {
+                console.log("you lose")
+                this.enemies.hide();
+                this.attack.attr("disabled", true);
+                this.reset.show();
+            }
+
+
+        },
+        nextFight: function(){
+            this.attack.attr("disabled",false);
+        },
+        showOnScreen:function(){
 
         }
     };
@@ -105,67 +131,75 @@ $(document).ready(function () {
     //=====================================================================================================
     window.onload = function () {
 
-        game.startplayer();
+        game.startGame();
     }
     // User selects a character
     $("#gokuchar").on("click", function () {
-        player.attack = 12;
-        player.startingAttack = 12;
+        player.attack = 6;
+        player.hit = 12;
         player.health = 140;
         game.selectGoku();
-        
+
     });
 
     $("#vegetachar").on("click", function () {
         player.attack = 10;
-        player.startingAttack = 8;
+        player.hit = 10;
         player.health = 100;
-        game.selectVegeta();  
+        game.selectVegeta();
     });
 
     $("#friezachar").on("click", function () {
-        player.attack = 10;
-        player.startingAttack = 8;
+        player.attack = 8;
+        player.hit = 8;
         player.health = 120;
         game.selectFrieza();
-        
+
     });
 
     $("#buuchar").on("click", function () {
         player.attack = 10;
-        player.startingAttack = 10;
+        player.hit = 10;
         player.health = 180;
         game.selectBuu();
-        
+
     });
 
     //User selects enemy
     $("#gokuen").on("click", function () {
         game.enCharts[0] = $("#gokuen")
+        enemy.hit = 50;
+        enemy.health = 1500;
+        game.nextFight();
         game.selectGokuEnemy();
-        
+
     });
 
     $("#vegetaen").on("click", function () {
         game.enCharts[1] = $("#vegetaen")
-        enemy.attack = 10;
-        enemy.startingAttack = 8;
-        enemy.health = 120;
-        game.selectVegetaEnemy();
-    });
-    $("#vegetaen").on("click", function () {
-        game.enCharts[1] = $("#vegetaen")
+        enemy.hit = 10;
+        enemy.health = 200;
+        game.nextFight();
         game.selectVegetaEnemy();
     });
     $("#friezaen").on("click", function () {
         game.enCharts[1] = $("#friezaen")
+        enemy.hit = 10;
+        enemy.health = 1000;
+        game.nextFight();
         game.selectFriezaEnemy();
     });
     $("#buuen").on("click", function () {
         game.enCharts[1] = $("#buuen")
+        enemy.hit = 40;
+        enemy.health = 1400;
+        game.nextFight();
         game.selectBuuEnemy();
     });
-    game.attack.click(function(){
+    game.attack.click(function () {
         game.fight();
     });
+    game.reset.click(function (){
+        location.reload();
+    })
 });
