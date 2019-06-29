8635470c84b8019f41c43a1904ac403ff4fd1a64
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-06-28 22:31:03 -0400

    working on fight and final Game over when game win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,14 +8,16 @@ $(document).ready(function () {
         health: 0,
         currentAttack: 0,
         userStats: $("#userStats"),
-        name: $("#player")
+        name: "",
+        healthDiplay: "",
     }
 
     var enemy = {
         counterAttack: 0,
         health: 0,
         enemyStats: $("#enemyStats"),
-        name: $("#enemy")
+        name: "",
+        healthDiplay: "",
     }
 
     var game = {
@@ -26,14 +28,22 @@ $(document).ready(function () {
         enCharts: [$("#gokuen"), $("#vegetaen"), $("#friezaen"), $("#buuen")],
         defCharts: [$("#gokudef"), $("#vegetadef"), $("#friezadef"), $("#buudef")],
         listOfNames:["Goku","Vegeta","Frieza","Maijin Buu"],
+        gokuHealth: $(".gokuHealth"),
+        vegetaHealth: $(".vegetaHealth"),
+        friezaHealth: $(".friezaHealth"),
+        buuHealth:  $(".buuHealth"),
         attack: $("#attack"),
         reset: $("#reset"),
         //Functions
         //==============================================================================================================================================
-        startGame: function () {
-            this.enemies.hide();
+        startGame: function (goku,vegeta,frieza,buu) {
+            // this.enemies.hide();
             this.defenders.hide();
             this.reset.hide();
+            this.gokuHealth.text(goku);
+            this.vegetaHealth.text(vegeta);
+            this.friezaHealth.text(frieza);
+            this.buuHealth.text(buu);
         },
 
         //This section manipulates the character selection
@@ -43,7 +53,9 @@ $(document).ready(function () {
             this.userChar[2].hide();
             this.userChar[3].hide();
             this.enemies.show();
-            this.enCharts[0].hide()
+            this.enCharts[0].hide();
+            
+            
         },
         selectVegeta: function () {
             this.userChar[0].hide();
@@ -100,28 +112,52 @@ $(document).ready(function () {
         },
         //function controls the fight outcome
         fight: function () {
+            player.userStats.text("You attacked " + enemy.name + " for " + player.baseAttack  + " damage");
+            enemy.enemyStats.text(enemy.name + " attacked you for " + enemy.counterAttack + " damage");
             enemy.health -= player.baseAttack;
             player.baseAttack += player.currentAttack;
-            console.log(enemy.health);
-            console.log(player.baseAttack);
             player.health -= enemy.counterAttack;
-            console.log(player.health);
-            player.userStats.text("Was Attacked for " + enemy.counterAttack + " damage remaining health " + player.health );
-            enemy.enemyStats.text("Attacked you for " + player.currentAttack + " damage remaining health " +enemy.health);
+            
+            
+            if (player.name === this.listOfNames[0]){
+                this.gokuHealth.text(player.health); 
+            }else if (player.name === this.listOfNames[1]){
+                this.vegetaHealth.text(player.health); 
+            }else if (player.name === this.listOfNames[2]){
+                this.friezaHealth.text(player.health);
+            }else if (player.name === this.listOfNames[3]){
+                this.buuHealth.text(player.health);
+            };
+
+            if (enemy.name === this.listOfNames[0]){
+                this.gokuHealth.text(enemy.health); 
+            }else if (enemy.name === this.listOfNames[1]){
+                this.vegetaHealth.text(enemy.health); 
+            }else if (enemy.name === this.listOfNames[2]){
+                this.friezaHealth.text(enemy.health);
+            }else if (enemy.name === this.listOfNames[3]){
+                this.buuHealth.text(enemy.health);
+            };
+
             if (enemy.health <= 0) {
-                console.log("pick a new character")
+                player.userStats.text("pick a new character")
+                enemy.enemyStats.text("");
                 this.defenders.hide();
                 this.attack.attr("disabled", true);
 
             } else if (player.health <= 0) {
-                console.log("you lose")
+                player.userStats.text("you lost");
+                enemy.enemyStats.text("");
                 this.enemies.hide();
                 this.attack.attr("disabled", true);
                 this.reset.show();
             }
-
-
-        },
+                if ((enCharts.length===1)){
+                    player.userStats.text("You Lost");
+                    this.reset.show();
+                }  
+        },   
+        
         nextFight: function(){
             this.attack.attr("disabled",false);
         },
@@ -130,76 +166,75 @@ $(document).ready(function () {
         setPlayerValue : function( baseAttack, currentAttack, health, name){
             player.baseAttack = baseAttack;
             player.currentAttack = currentAttack;
-            player.health = health;
-            player.name.text(name);
+            player.health = health; 
+            player.name = name;
         },
         
         // _____________________ ///
         setEnemyValue : function( counterAttack, health, name){
             enemy.counterAttack = counterAttack;
             enemy.health = health;
-            enemy.name.text(name);
-        }
+            enemy.name = name;
+        },
     };
 
     //User interaction
     //=====================================================================================================
     window.onload = function () {
 
-        game.startGame();
+        game.startGame(120,100,150,180);
     }
     // User selects a character
     $("#gokuchar").on("click", function () {
-        game.setPlayerValue(6,6,140,game.listOfNames[0]);
+        game.setPlayerValue(6,6,120,game.listOfNames[0]);
         game.selectGoku();
+        
     });
 
     $("#vegetachar").on("click", function () {
-        player.name.text(game.listOfNames[1]);
-        game.setPlayerValue(10,10,100);
+
+        game.setPlayerValue(10,10,100,game.listOfNames[1]);
         game.selectVegeta();
     });
 
     $("#friezachar").on("click", function () {
-        player.name.text(game.listOfNames[2]);
-        game.setPlayerValue(8,8,120);
+        game.setPlayerValue(8,8,150,game.listOfNames[2]);
         game.selectFrieza();
 
     });
 
     $("#buuchar").on("click", function () {
-        player.name.text(game.listOfNames[3]);
-        game.setPlayerValue(10,10,180);
+        game.setPlayerValue(10,10,180,game.listOfNames[3]);
         game.selectBuu();
 
     });
 
     //User selects enemy
     $("#gokuen").on("click", function () {
-        enemy.name.text(game.listOfNames[1]);
-        game.setEnemyValue(6,140);
-        game.nextFight();
+        game.setEnemyValue(6,120,game.listOfNames[0]);
         game.selectGokuEnemy();
+        // enCharts.splice(0,0);
+        game.nextFight();
 
     });
 
     $("#vegetaen").on("click", function () {
-        enemy.name.text(game.listOfNames[1]);
-        game.setEnemyValue(10, 100,"Vegeta");
-        game.nextFight();
+        game.setEnemyValue(10, 100,game.listOfNames[1]);
         game.selectVegetaEnemy();
+        // enCharts.splice(1,1);
+        game.nextFight();
     });
     $("#friezaen").on("click", function () {
-        enemy.name.text(game.listOfNames[2]);
-        game.setEnemyValue(8, 120);
-        game.nextFight();
+        game.setEnemyValue(8, 150,game.listOfNames[2]);
         game.selectFriezaEnemy();
+        // enCharts.splice(2,2);
+        game.nextFight();
     });
     $("#buuen").on("click", function () {
-        enemy.name.text(game.listOfNames[3]);
-        game.setEnemyValue(10,180);
-        game.nextFight();
+        game.setEnemyValue(10,180,game.listOfNames[3]);
         game.selectBuuEnemy();
+        // enCharts.splice(3,3);
+        game.nextFight();
     });
     game.attack.click(function () {
         game.fight();
